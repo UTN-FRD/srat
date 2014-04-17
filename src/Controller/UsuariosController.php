9ece7b16feb81078568ba8a0fe0a91944bb924b4
jalbertocr <jalberto.cr@live.com>
2014-04-17 19:36:04 -0300

    Agregado controlador Usuarios.

---
+++ src/Controller/UsuariosController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Sistema de Registro de Asistenca y Temas
+ *
+ * (c) Universidad Tecnológica Nacional - Facultad Regional Delta
+ *
+ * Este archivo está sujeto a los términos y condiciones descritos
+ * en el archivo licencia.txt que acompaña a este software.
+ *
+ * @author Jorge Alberto Cricelli <jalberto.cr@live.com>
+ */
+
+/**
+ * Dependencias
+ */
+App::uses('AppController', 'Controller');
+
+/**
+ * Usuarios
+ *
+ * @author Jorge Alberto Cricelli <jalberto.cr@live.com>
+ */
+class UsuariosController extends AppController {
+
+/**
+ * Dashboard
+ *
+ * @return void
+ */
+	public function dashboard() {
+		$this->set('title_for_layout', 'Dashboard');
+	}
+}
