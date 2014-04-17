a1770a43154f1e36d415fefb23b91b06ea83ecfe
jalbertocr <jalberto.cr@live.com>
2014-04-17 04:08:42 -0300

    Actualizada configuración del arranque de la aplicación.

--- src/Config/bootstrap.php
+++ src/Config/bootstrap.php
@@ -1,98 +1,80 @@
 <?php
 /**
- * This file is loaded automatically by the app/webroot/index.php file after core.php
+ * Configuración del arranque de la aplicación
  *
- * This file should load/create any application wide configuration settings, such as
- * Caching, Logging, loading additional configuration files.
+ * Sistema de Registro de Asistenca y Temas
  *
- * You should also use this file to include any files that provide global functions/constants
- * that your application uses.
+ * (c) Universidad Tecnológica Nacional - Facultad Regional Delta
  *
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.Config
- * @since         CakePHP(tm) v 0.10.8.2117
- */
-
-// Setup a 'default' cache configuration for use in the application.
-Cache::config('default', array('engine' => 'File'));
-
-/**
- * The settings below can be used to set additional paths to models, views and controllers.
+ * Este archivo está sujeto a los términos y condiciones descritos
+ * en el archivo licencia.txt que acompaña a este software.
  *
- * App::build(array(
- *     'Model'                     => array('/path/to/models/', '/next/path/to/models/'),
- *     'Model/Behavior'            => array('/path/to/behaviors/', '/next/path/to/behaviors/'),
- *     'Model/Datasource'          => array('/path/to/datasources/', '/next/path/to/datasources/'),
- *     'Model/Datasource/Database' => array('/path/to/databases/', '/next/path/to/database/'),
- *     'Model/Datasource/Session'  => array('/path/to/sessions/', '/next/path/to/sessions/'),
- *     'Controller'                => array('/path/to/controllers/', '/next/path/to/controllers/'),
- *     'Controller/Component'      => array('/path/to/components/', '/next/path/to/components/'),
- *     'Controller/Component/Auth' => array('/path/to/auths/', '/next/path/to/auths/'),
- *     'Controller/Component/Acl'  => array('/path/to/acls/', '/next/path/to/acls/'),
- *     'View'                      => array('/path/to/views/', '/next/path/to/views/'),
- *     'View/Helper'               => array('/path/to/helpers/', '/next/path/to/helpers/'),
- *     'Console'                   => array('/path/to/consoles/', '/next/path/to/consoles/'),
- *     'Console/Command'           => array('/path/to/commands/', '/next/path/to/commands/'),
- *     'Console/Command/Task'      => array('/path/to/tasks/', '/next/path/to/tasks/'),
- *     'Lib'                       => array('/path/to/libs/', '/next/path/to/libs/'),
- *     'Locale'                    => array('/path/to/locales/', '/next/path/to/locales/'),
- *     'Vendor'                    => array('/path/to/vendors/', '/next/path/to/vendors/'),
- *     'Plugin'                    => array('/path/to/plugins/', '/next/path/to/plugins/'),
- * ));
- *
+ * @author Jorge Alberto Cricelli <jalberto.cr@live.com>
  */
 
 /**
- * Custom Inflector rules can be set to correctly pluralize or singularize table, model, controller names or whatever other
- * string is passed to the inflection functions
- *
- * Inflector::rules('singular', array('rules' => array(), 'irregular' => array(), 'uninflected' => array()));
- * Inflector::rules('plural', array('rules' => array(), 'irregular' => array(), 'uninflected' => array()));
- *
- */
-
-/**
- * Plugins need to be loaded manually, you can either load them one by one or all of them in a single call
- * Uncomment one of the lines below, as you need. Make sure you read the documentation on CakePlugin to use more
- * advanced ways of loading plugins
- *
- * CakePlugin::loadAll(); // Loads all plugins at once
- * CakePlugin::load('DebugKit'); //Loads a single plugin named DebugKit
- *
- */
-
-/**
- * You can attach event listeners to the request lifecycle as Dispatcher Filter . By default CakePHP bundles two filters:
- *
- * - AssetDispatcher filter will serve your asset files (css, images, js, etc) from your themes and plugins
- * - CacheDispatcher filter will read the Cache.check configure variable and try to serve cached content generated from controllers
- *
- * Feel free to remove or add filters as you see fit for your application. A few examples:
- *
- * Configure::write('Dispatcher.filters', array(
- *		'MyCacheFilter', //  will use MyCacheFilter class from the Routing/Filter package in your app.
- *		'MyPlugin.MyFilter', // will use MyFilter class from the Routing/Filter package in MyPlugin plugin.
- * 		array('callable' => $aFunction, 'on' => 'before', 'priority' => 9), // A valid PHP callback type to be called on beforeDispatch
- *		array('callable' => $anotherMethod, 'on' => 'after'), // A valid PHP callback type to be called on afterDispatch
- *
- * ));
- */
-Configure::write('Dispatcher.filters', array(
-	'AssetDispatcher',
-	'CacheDispatcher'
-));
-
-/**
- * Configures default file logging options
+ * Dependencias
  */
 App::uses('CakeLog', 'Log');
-CakeLog::config('debug', array(
-	'engine' => 'File',
-	'types' => array('notice', 'info', 'debug'),
-	'file' => 'debug',
+
+/**
+ * Localidad
+ */
+setlocale(LC_ALL, 'es_ES.UTF-8', 'esp');
+
+/**
+ * Zona horaria
+ */
+date_default_timezone_set(Configure::read('Config.timezone'));
+
+/**
+ * Reglas de inflexión para idioma español
+ */
+Inflector::rules('singular', array(
+	'rules' => array(
+		'/(ll)es$/i' => '\1e',
+		'/([r|d|j|n|l|m|y|z])es$/i' => '\1',
+		'/as$/i' => 'a',
+		'/([ti])a$/i' => '\1a'
+	)
 ));
+Inflector::rules('plural', array(
+	'rules' => array(
+		'/([r|d|j|n|l|m|y|z])$/i' => '\1es',
+		'/a$/i' => '\1as'
+	)
+));
+
+/**
+ * Registro de depuración
+ */
+CakeLog::config('debug', array(
+	'engine' => 'FileLog',
+	'file' => 'debug',
+	'scopes' => array('debug', 'info', 'notice')
+));
+
+/**
+ * Registro de error
+ */
 CakeLog::config('error', array(
-	'engine' => 'File',
-	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
+	'engine' => 'FileLog',
 	'file' => 'error',
+	'scopes' => array('alert', 'critical', 'emergency', 'error', 'warning')
+));
+
+/**
+ * Filtros del despachador
+ */
+Configure::write('Dispatcher.filters', array(
+	'AssetDispatcher'
+));
+
+/**
+ * Configuración del cache
+ */
+Cache::config('default', array(
+	'duration' => '+12 months',
+	'engine' => 'File',
+	'prefix' => APP_DIR . '_default_'
 ));
