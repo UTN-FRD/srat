690f71ec3a3a47eed80cbef2a7a4c79915e271a1
jalbertocr <jalberto.cr@live.com>
2014-04-22 16:18:20 -0300

    Agregado MyHtmlHelper::parseLinkList() para generar elementos de una lista HTML compatible con la librería Bootstrap.

--- src/View/Helper/MyHtmlHelper.php
+++ src/View/Helper/MyHtmlHelper.php
@@ -21,4 +21,125 @@ App::uses('HtmlHelper', 'View/Helper');
  * @author Jorge Alberto Cricelli <jalberto.cr@live.com>
  */
 class MyHtmlHelper extends HtmlHelper {
+
+/**
+ * Procesa enlaces para una lista HTML
+ *
+ * ### Opciones del enlace
+ *
+ * - (array) `attribs`
+ * Opciones para el elemento LI pasadas a `MyHtmlHelper::tag()`
+ *
+ * - (boolean) `condition`
+ * El enlace no se procesa si el valor no es verdadero
+ *
+ * - (boolean) `divider`
+ * Indica que el elemento es un divisor y no un enlace
+ *
+ * - (array) `dropdown`
+ * Uno o más enlaces a mostrar en un menú desplegable
+ *
+ * - (boolean) `header`
+ * Indica que el elemento es una cabecera y no un enlace
+ *
+ * - (array) `options`
+ * Opciones para el enlace pasadas a `MyHtmlHelper::link()`
+ *
+ * - (string) `text`
+ * Texto del enlace
+ *
+ * - (array|string) `url`
+ * Dirección URL
+ *
+ * @param array $links Enlaces
+ *
+ * @return string Cada enlace dentro de un elemento LI o una cadena vacía en caso contrario
+ */
+	public function parseLinkList($links) {
+		static $depth = 0;
+		$depth++;
+
+		$out = '';
+		foreach ($links as $link) {
+			if (!is_array($link)) {
+				continue;
+			}
+
+			$link += array(
+				'attribs' => array(),
+				'condition' => true,
+				'divider' => false,
+				'dropdown' => array(),
+				'header' => false,
+				'options' => array(),
+				'text' => null,
+				'url' => '#'
+			);
+
+			if (!$link['condition']) {
+				continue;
+			} elseif ($link['divider']) {
+				$link['divider'] = 'divider';
+				if ($depth <= 1) {
+					$link['divider'] .= '-vertical';
+				}
+				$out .= $this->tag('li', '', array('class' => $link['divider']));
+			} elseif ($link['header'] && !empty($link['text'])) {
+				$out .= $this->tag('li', h($link['text']), array('class' => 'nav-header'));
+			} else {
+				if (empty($link['text']) ||	(empty($link['url']) && empty($link['dropdown']))) {
+					continue;
+				}
+
+				if ($depth > 1 || empty($link['dropdown'])) {
+					$link['dropdown'] = false;
+				} else {
+					$link['dropdown'] = $this->parseLinkList($link['dropdown']);
+					if ($link['dropdown']) {
+						$link['dropdown'] = $this->tag('ul', $link['dropdown'], array('class' => 'dropdown-menu'));
+
+						if (!isset($link['attribs']['class'])) {
+							$link['attribs']['class'] = 'dropdown';
+						} else {
+							$link['attribs']['class'] .= ' dropdown';
+						}
+
+						if (!isset($link['options']['class'])) {
+							$link['options']['class'] = 'dropdown-toggle';
+						} else {
+							$link['options']['class'] .= ' dropdown-toggle';
+						}
+
+						$link['options']['data-toggle'] = 'dropdown';
+						$link['url'] = '#';
+					}
+				}
+
+				if ($link['dropdown']) {
+					if (!isset($link['options']['escape'])) {
+						$link['options']['escape'] = true;
+					}
+
+					if ($link['options']['escape']) {
+						$link['text'] = h($link['text']);
+					}
+					$link['options']['escape'] = false;
+
+					if ($link['dropdown']) {
+						$link['text'] .= ' ' . $this->tag('b', '', array('class' => 'caret'));
+					}
+				}
+
+				$link['text'] = $this->link($link['text'], $link['url'], $link['options']);
+				if ($link['dropdown']) {
+					$link['text'] .= $link['dropdown'];
+				}
+
+				$out .= $this->tag('li', $link['text'], $link['attribs']);
+			}
+		}
+
+		$depth--;
+		return $out;
+	}
 }
