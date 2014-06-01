17414a15a217e9e2b2cbf8cfaffbc50ab732ae88
jalbertocr <jalberto.cr@live.com>
2014-05-31 23:34:01 -0300

    Agregada shell Backup.

---
+++ src/Console/Command/BackupShell.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Sistema de Registro de Asistencia y Temas
+ *
+ * (c) Universidad Tecnológica Nacional - Facultad Regional Delta
+ *
+ * Este archivo está sujeto a los términos y condiciones descritos
+ * en el archivo licencia.txt que acompaña a este software.
+ *
+ * @author Jorge Alberto Cricelli <jalberto.cr@live.com>
+ */
+
+/**
+ * Dependencias
+ */
+App::uses('AppShell', 'Console');
+App::uses('ConnectionManager', 'Model');
+
+/**
+ * Genera una copia de seguridad de la base de datos
+ *
+ * @author Jorge Alberto Cricelli <jalberto.cr@live.com>
+ */
+class BackupShell extends AppShell {
+
+/**
+ * main()
+ *
+ * @return void
+ */
+	public function main() {
+	}
+}
