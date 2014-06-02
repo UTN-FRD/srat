d043b9b88eb262dc786481f6d3a79a15f94f82d2
jalbertocr <jalberto.cr@live.com>
2014-06-02 00:35:21 -0300

    Completada shell Backup.

--- src/Console/Command/BackupShell.php
+++ src/Console/Command/BackupShell.php
@@ -24,11 +24,116 @@ App::uses('ConnectionManager', 'Model');
 class BackupShell extends AppShell {
 
 /**
- * main()
+ * Configuración
+ *
+ * @var array
+ */
+	protected $_config = array();
+
+/**
+ * Genera una copia de seguridad de la base de datos
  *
  * @return void
+ *
+ * @throws RuntimeException Si no puede cargarse el archivo de configuración
  */
 	public function main() {
+		try {
+			if (!Configure::load('backup')) {
+				throw new RuntimeException('No fue posible cargar el archivo de configuración.');
+			}
+
+			$this->_config = array_merge(
+				(array)Configure::read('Backup'),
+				ConnectionManager::getDataSource('default')->config
+			);
+		} catch (Exception $e) {
+			$this->error($e->getMessage());
+		}
+
+		$filename = $this->__getOutputFile();
+		if (!$filename) {
+			$this->error(sprintf("La ruta de acceso '%s' no existe o no es accesible.", $this->_config['dest']));
+		}
+
+		$command = $this->__getBackupCommand($filename);
+		if (!$command) {
+			$this->error("La aplicación 'mysqldump' no existe o no es accesible.");
+		}
+
+		exec($command, $output, $status);
+		if ($status === 0) {
+			if ($this->_config['compress']) {
+				if ($this->__gzipFile($filename)) {
+					unlink($filename);
+				}
+			}
+			$this->out('Se ha realizado la copia de seguridad.');
+		} else {
+			$this->hr();
+			$this->out('No fue posible realizar la copia de seguridad.');
+		}
+	}
+
+/**
+ * Obtiene la ruta de acceso al archivo de la copia de seguridad
+ *
+ * @return boolean|string Ruta de acceso al archivo o `false` en caso contrario
+ */
+	private function __getOutputFile() {
+		if (
+			empty($this->_config['dest']) || !is_dir($this->_config['dest']) ||
+			!is_writable($this->_config['dest'])
+		) {
+			return false;
+		}
+
+		$this->_config['filename'] = strftime($this->_config['filename']);
+		if (!$this->_config['filename']) {
+			$this->_config['filename'] = strftime('srat_%Y-%m-%d');
+		}
+
+		return rtrim($this->_config['dest'], '/\\') . DS . $this->_config['filename'] . '.sql';
+	}
+
+/**
+ * Obtiene el comando a ejecutar para realizar la copia de seguridad
+ *
+ * @param string $filename Ruta de acceso al archivo
+ *
+ * @return boolean|string Comando o `false` en caso contrario
+ */
+	private function __getBackupCommand($filename) {
+		if (
+			empty($filename) || empty($this->_config['mysqldump']) ||
+			!is_executable($this->_config['mysqldump'])
+		) {
+			return false;
+		}
+
+		$command = array(
+			$this->_config['mysqldump']
+		);
+		if (!empty($this->_config['host'])) {
+			$command[] = '--host=' . $this->_config['host'];
+		}
+		if (!empty($this->_config['port'])) {
+			$command[] = '--port=' . $this->_config['port'];
+		}
+		if (!empty($this->_config['login'])) {
+			$command[] = '--user=' . $this->_config['login'];
+		}
+		if (!empty($this->_config['password'])) {
+			$command[] = '--password=' . $this->_config['password'];
+		}
+		if (!empty($this->_config['options'])) {
+			$command[] = $this->_config['options'];
+		}
+		if (!empty($this->_config['database'])) {
+			$command[] = $this->_config['database'];
+		}
+
+		return implode($command, ' ') . ' > ' . $filename;
 	}
 
 /**
