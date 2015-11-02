0918ea51180b8c7b8934afc1851f58a86b324ff3
Jorge Alberto Cricelli <jacricelli@gmail.com>
2015-11-02 09:01:26 -0300

    Refactorizada shell Registros, agregado comando para eliminar registros por tipo y documentación adicional.

--- app/Console/Command/RegistrosShell.php
+++ app/Console/Command/RegistrosShell.php
@@ -30,22 +30,111 @@ class RegistrosShell extends AppShell {
 	public $uses = array('Registro');
 
 /**
- * Elimina el registro de asistencia
+ * Elimina registros
+ *
+ * Uso:
+ * cake registros eliminar [-a] [-i] [-p]
+ *
+ * Opciones:
+ *
+ * --asistencia, -a    Registros de asistencia
+ * --inasistencia, -i  Registros de inasistencia
+ * --no-laborable, -p  Registros cuya fecha coincide con un día no laborable
  *
  * @return void
  */
 	public function eliminar() {
+		$conditions = array();
+		foreach (array('inasistencia', 'asistencia', 'no-laborable') as $key => $type) {
+			if ($this->param($type)) {
+				$conditions[] = $key;
+			}
+		}
+		if (empty($conditions)) {
+			$this->error('Especifique al menos un tipo de registro.');
+		}
+
 		$option = strtoupper($this->in(
-			'¿Está seguro que desea eliminar el registro de asistencia?',
+			'¿Está seguro que desea eliminar los registros?',
 			array('S', 'N'),
 			'N'
 		));
 		if ($option === 'S') {
-			if ($this->Registro->getDataSource()->truncate($this->Registro->useTable)) {
+			$result = false;
+			if (count($conditions) == 3) {
+				$result = $this->Registro->getDataSource()->truncate($this->Registro->useTable);
+			} else {
+				$this->Registro->unbindModel(array(
+					'belongsTo' => array_keys($this->Registro->belongsTo),
+					'hasOne' => array_keys($this->Registro->hasOne),
+				));
+				$result = $this->Registro->deleteAll(array('Registro.tipo' => $conditions), false, false);
+			}
+			if ($result) {
 				$this->out('La operación solicitada se ha completado exitosamente.');
 			} else {
-				$this->error('No fue posible eliminar el registro de asistencia debido a un error interno.');
+				$this->error('La operación solicitada no se ha completado debido a un error interno.');
 			}
 		}
+	}
+
+/**
+ * Trunca la tabla de registros
+ *
+ * Uso:
+ * cake registros truncar
+ *
+ * @return void
+ */
+	public function truncar() {
+		$option = strtoupper($this->in(
+			'¿Está seguro que desea truncar la tabla de registros?',
+			array('S', 'N'),
+			'N'
+		));
+		if ($option === 'S') {
+			$result = $this->Registro->getDataSource()->truncate($this->Registro->useTable);
+			if ($result) {
+				$this->out('La operación solicitada se ha completado exitosamente.');
+			} else {
+				$this->error('La operación solicitada no se ha completado debido a un error interno.');
+			}
+		}
+	}
+
+/**
+ * getOptionParser
+ *
+ * @return ConsoleOptionParser
+ */
+	public function getOptionParser() {
+		$parser = parent::getOptionParser();
+		$parser->addSubcommand('eliminar', array(
+			'help' => 'Elimina registros',
+			'parser' => array(
+				'options' => array(
+					'asistencia' => array(
+						'boolean' => true,
+						'help' => 'Registros de asistencia',
+						'short' => 'a'
+					),
+					'inasistencia' => array(
+						'boolean' => true,
+						'help' => 'Registros de inasistencia',
+						'short' => 'i'
+					),
+					'no-laborable' => array(
+						'boolean' => true,
+						'help' => 'Registros cuya fecha coincide con un día no laborable',
+						'short' => 'p'
+					)
+				)
+			)
+		))->addSubcommand('truncar', array(
+			'help' => 'Trunca la tabla de registros'
+		))->description(
+			'Gestión de registros'
+		);
+		return $parser;
 	}
 }
